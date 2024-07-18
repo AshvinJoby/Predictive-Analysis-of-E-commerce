899e82ffc94a0958b86782c474ae8b5f41722164
Ashvin Joby <ashvinjoby12345@gmail.com>
2024-07-18 22:39:43 +0530

    Added functionality for backend

---
+++ main.py
@@ -0,0 +1,142 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import cross_val_score, KFold, train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, Normalizer
+from sklearn.metrics import confusion_matrix, f1_score, accuracy_score, roc_auc_score
+from sklearn.feature_selection import SelectKBest
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import LabelEncoder
+
+df = pd.read_excel('Dataset.xlsx', sheet_name = 'E Comm')
+df.head()
+dt=df.copy()
+df.describe()
+df.info()
+
+sns.set(style="darkgrid", palette="muted")
+fig, ax = plt.subplots(figsize=(16,7))
+sns.boxplot(data=df)
+plt.xticks(rotation=45)
+plt.show()
+
+cat = df.select_dtypes(include='object').columns
+num = list(df.select_dtypes(exclude='object').columns)
+num.remove('Churn')
+for cols in num:
+    Q1 = df[cols].quantile(0.25)
+    Q3 = df[cols].quantile(0.75)
+    IQR=Q3-Q1
+    lr= Q1-(1.5 * IQR)
+    ur= Q3+(1.5 * IQR)
+    df[cols] = df[cols].mask(df[cols]<lr, lr, )
+    df[cols] = df[cols].mask(df[cols]>ur, ur, )
+
+sns.set_theme(style="darkgrid", palette="muted")
+fig, ax = plt.subplots(figsize=(16,7))
+sns.boxplot(data=df)
+plt.xticks(rotation=45)
+plt.show()
+
+df.isnull().sum()
+df['Tenure'].fillna(df.Tenure.median(), inplace=True)
+df['WarehouseToHome'].fillna(df.WarehouseToHome.median(), inplace=True)
+df['HourSpendOnApp'].fillna(df.HourSpendOnApp.median(), inplace=True)
+df['OrderAmountHikeFromlastYear'].fillna(round(df.OrderAmountHikeFromlastYear.mean()), inplace=True)
+df['CouponUsed'].fillna(df.CouponUsed.median(), inplace=True)
+df['OrderCount'].fillna(df.OrderCount.median(), inplace=True)
+df['DaySinceLastOrder'].fillna(df.DaySinceLastOrder.median(), inplace=True)
+
+df.isnull().sum()
+
+for col in cat:
+    print(df[col].value_counts())
+fig,ax = plt.subplots(nrows=2,ncols=3,figsize=(26,12))
+for col,subplot in zip(cat, ax.flatten()):
+  sns.countplot(x = df[col], hue=df.Churn, ax=subplot)
+fig, ax = plt.subplots(2, 3, figsize=(30, 20))
+plt.rcParams['font.size'] = '16'
+for col,subplot in zip(cat, ax.flatten()):
+    temp = df.groupby(by=df[col]).Churn.sum()
+    total = df.value_counts(col).sort_index()
+    res1 = temp/total*100
+    subplot.pie(labels = res1.index, x = res1.values, autopct='%.0f%%',textprops={'fontsize': 16})
+for i, subplot in zip(num, ax.flatten()):
+    sns.histplot(df[i], kde = True, ax=subplot)
+fig, ax = plt.subplots(2, 6, figsize=(30, 10))
+for col,subplot in zip(num, ax.flatten()):
+    temp = df.groupby(by=df[col]).Churn.sum()
+    total = df.value_counts(col).sort_index()
+    res1 = temp/total*100
+    sns.lineplot(x = res1.index, y = res1.values, ax=subplot, )
+    subplot.tick_params(axis='x',labelrotation=45)
+
+numeric_df = df.select_dtypes(include=[np.number])
+mask=np.zeros_like(numeric_df.corr())
+mask[np.triu_indices_from(mask)] = True
+fig, ax = plt.subplots( figsize=(16, 7))
+sns.heatmap(numeric_df.corr(method='pearson'), mask=mask, cmap='rainbow')
+
+enc = LabelEncoder()
+for col in df.select_dtypes(include='object'):
+    df[col]=enc.fit_transform(df[col])
+
+X_train, X_test,y_train, y_test = train_test_split(df.drop('Churn', axis=1), df.Churn)
+
+class my_classifier(BaseEstimator,):
+    def __init__(self, estimator=None):
+        self.estimator = estimator
+    def fit(self, X, y=None):
+        self.estimator.fit(X,y)
+        return self
+    def predict(self, X, y=None):
+        return self.estimator.predict(X,y)
+    def predict_proba(self, X):
+        return self.estimator.predict_proba(X)
+    def score(self, X, y):
+        return self.estimator.score(X, y)
+    
+pipe = Pipeline([('scaler', StandardScaler()), ('clf', my_classifier())])
+
+parameters = [
+              {'clf':[LogisticRegression(max_iter=1000)],
+               'clf__C':[0.001,0.01,.1,1],
+               'clf__solver':['lbfgs','liblinear']
+               },
+             {'clf':[RandomForestClassifier()],
+             'clf__criterion':['gini','entropy'],
+             },
+             {
+               'clf':[DecisionTreeClassifier()],
+             'clf__criterion':['gini','entropy'],
+             },
+             {
+              'clf':[XGBClassifier()],
+             'clf__learning_rate':[0.01,0.1,0.2,0.3],
+             'clf__reg_lambda':[0.01,0.1,1],
+             'clf__reg_alpha': [0.01,0.1,0,1],
+             }]
+
+grid = GridSearchCV(pipe, parameters, cv=5)
+grid.fit(X_train,y_train)
+grid.best_estimator_
+grid.best_score_
+y_pred = grid.predict(X_test,)
+
+sns.heatmap(confusion_matrix(y_test, y_pred), annot=True)
+print(f1_score(y_test,y_pred))
+
+feature_array = grid.best_estimator_[-1].feature_importances_
+importance = dict(zip(df.drop('Churn',axis=1).columns,feature_array))
+importance = dict(sorted(importance.items(), key= lambda item:item[1],reverse = True) )
+fig, ax = plt.subplots(figsize=(26,8))
+sns.barplot(x=list(importance.keys()), y=list(importance.values()))
+plt.tick_params(axis='x', labelrotation=45)
+plt.show()
